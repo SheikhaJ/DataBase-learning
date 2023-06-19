6235db096e6d8e6f8800f5b2490222df5777aafe
SheikhaJ <120921603+SheikhaJ@users.noreply.github.com>
2023-06-19 12:09:05 +0400

    Add files via upload

---
+++ advanced indexer/advanced indexer/FavBook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advanced_indexer
+{
+    internal class FavBook
+    {
+        private string[] BookName;
+        private string[] Author;
+        private int len;
+
+        public FavBook(int len)
+        {
+            this.len = len;
+            BookName = new string[len];
+            Author = new string[len];
+        }
+        public string this[int index, string name]
+        {
+            set
+            {
+                BookName[index] = name;
+                Author[index] = value;
+            }
+            get {
+                string res = $"{name} : {Author[index]}";
+                return res ;
+            }
+        }
+    }
+}
---
+++ advanced indexer/advanced indexer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace advanced_indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FavBook book = new FavBook(2);
+            book[0, "aaa"] = "bbb";
+            book[1, "sss"] = "rrr";
+            Console.WriteLine(book[0,"aaa"]);
+        }
+    }
+}
