ab188d236fe3ee77d322f739541cee8b00fd2a84
SheikhaJ <120921603+SheikhaJ@users.noreply.github.com>
2023-06-19 12:07:35 +0400

    Add files via upload

---
+++ ApplicationDbContext/ApplicationDbContext/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationDbContext
+{
+    internal class ApplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Company;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+
+        }
+
+        DbSet<Employee> employees { get; set; }
+
+    }
+}
---
+++ ApplicationDbContext/ApplicationDbContext/Migrations/20230619063505_EmployeeAdded.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApplicationDbContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeeAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
